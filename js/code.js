4facb267016513373bd95de172f9e7d520663ecc
RomPop76 <r.popielas@gmail.com>
2023-04-25 23:07:59 +0200

    Commit 27

--- js/code.js
+++ js/code.js
@@ -1,7 +1,19 @@
 $(document).on('scroll', function() {
+    const windowHeight = $(window).height();
     const scroollValue = $(this).scrollTop();
     const $aboutText = $('.aboutText');
+    const $aboutImage = $('.aboutImage');
     const  aboutTextFromTop = $aboutText.offset().top;
     const aboutTextHeight = $aboutText.outerHeight();
-    console.log(aboutTextHeight);
+
+    if (scroollValue > aboutTextFromTop + aboutTextHeight - windowHeight) {
+        $aboutText.addClass('active');
+        $aboutImage.addClass('active');
+    }
+
+    if (scroollValue < 100) {
+        $('.aboutText').removeClass('active');
+        $('.aboutImage').removeClass('active');
+    }
+    
 })
